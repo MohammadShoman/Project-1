cd685e69ccc022c590b62c882ef93302a00d60ce
MohammadShoman <mohammad.shoman25@gmail.com>
2021-04-24 22:20:46 -0700

    added potions and color whene answers

--- Quiz.js
+++ Quiz.js
@@ -1,125 +1,147 @@
+console.log("Ouiz");
 
-console.log("Ouiz")
+const animalsQuestions = [
+  {
+    question: "What is the biggest animal that has ever lived ?",
+    options: [
+      "A) Blue whale",
+      "B) African elephant",
+      "C) Apatosaurus (aka Brontosaurus)",
+      "D) Spinosaurus",
+    ],
+    answer: 0,
+  },
 
+  {
+    question: "Which of these animals lives the longest?",
+    options: [
+      "A) Ocean quahog (clam)",
+      "B) Red sea urchin",
+      "C) Galapagos tortoise",
+      "D) Rougheye rockfish",
+    ],
+    answer: 0,
+  },
 
+  {
+    question: "What existing bird has the largest wingspan?",
+    options: ["A) Stork", "B) Swan", "C) Condor", "D) Albatross"],
+    answer: 3,
+  },
+  {
+    question: "Which of the following animals sleeps standing up?",
+    options: ["A) Gorillas", "B) Flamingos", "C) Hedgehogs", "D) Ravens"],
+    answer: 1,
+  },
+  {
+    question: "What is the fastest water animal?",
+    options: ["A) Porpoise", "B) Sailfish", "C) Flying fish", "D) Tuna"],
+    answer: 1,
+  },
+];
 
+//console.log(animalsQuestions[0])
 
-const animalsQuestions=[
-    {question:"What is the biggest animal that has ever lived ?",
-    options:["A) Blue whale","B) African elephant","C) Apatosaurus (aka Brontosaurus)","D) Spinosaurus"],
-    answer:0
-},
+const options = document.querySelector(".all");
 
-    {question:"Which of these animals lives the longest?",
-        options:["A) Ocean quahog (clam)","B) Red sea urchin","C) Galapagos tortoise","D) Rougheye rockfish"],
-        answer:0
-    },
+let currentQuestion;
+let allAnimalsQuestions = [];
+let allAnimalsOptions = [];
+let counter = animalsQuestions.length;
+let score=""
+let scoreCounter=0
 
-    {
-        question:"What existing bird has the largest wingspan?",
-        options:["A) Stork",
-            "B) Swan",
-            "C) Condor" ,
-            "D) Albatross"],
-        answer:3
-    },
-    {
-        question:"Which of the following animals sleeps standing up?",
-        options:["A) Gorillas",
-            "B) Flamingos",
-            "C) Hedgehogs" ,
-            "D) Ravens"],
-        answer:1
-    },
-    {
-        question:"What is the fastest water animal?",
-        options:["A) Porpoise",
-            "B) Sailfish",
-            "C) Flying fish" ,
-            "D) Tuna"],
-        answer:1
-    }
-]
+const setAllAnimalsQuestions = () => {
+  const totalAnimalsQuestions = animalsQuestions.length;
+  for (i = 0; i < totalAnimalsQuestions; i++) {
+    allAnimalsQuestions.push(animalsQuestions[i]);
+  }
+};
 
+const getQuestions = () => {
+  //Questions
+  options.innerHTML = "";
+  const questionNumber =
+    allAnimalsQuestions[Math.floor(Math.random() * allAnimalsQuestions.length)];
+  currentQuestion = questionNumber;
+  // console.log(currentQuestion)
+  const questions = document.createElement("h2");
+  questions.innerHTML = currentQuestion.question;
+  questions.className = "Question";
+  options.appendChild(questions);
 
-    //console.log(animalsQuestions[0])
+  const index = allAnimalsQuestions.indexOf(questionNumber);
+  allAnimalsQuestions.splice(index, 1);
 
+  //console.log(questionNumber)
+  // console.log(allAnimalsQuestions)
 
-    const questions=document.querySelector("h2")
-    
-    const options=document.querySelector(".all")
+  //-----------------------------------------------------------------------------------------------------
+  //Options
+  const allOptions = currentQuestion.options.length;
+  for (i = 0; i < allOptions; i++) {
+    allAnimalsOptions.push(i);
+  }
 
-    let currentQuestion;
-    let allAnimalsQuestions=[]
-    let allAnimalsOptions=[]
-    let counter=animalsQuestions.length;
-
-
-
-    const setAllAnimalsQuestions=()=>{
-        const totalAnimalsQuestions=animalsQuestions.length;
-        for(i=0;i<totalAnimalsQuestions;i++){
-            allAnimalsQuestions.push(animalsQuestions[i])
-        }
-    }
-
-
-
-    const getQuestions=()=>{
-        //Questions
-        const questionNumber=allAnimalsQuestions[Math.floor(Math.random()*allAnimalsQuestions.length)]
-        currentQuestion=questionNumber
-       // console.log(currentQuestion)
-
-        questions.innerHTML=currentQuestion.question;
-        const index= allAnimalsQuestions.indexOf(questionNumber)
-        allAnimalsQuestions.splice(index, 1)
-        //console.log(questionNumber)
-       // console.log(allAnimalsQuestions)
-        
-       //-----------------------------------------------------------------------------------------------------
-       //Options
-        const allOptions =currentQuestion.options.length
-        for(i=0;i<allOptions;i++){
-            allAnimalsOptions.push(i)
-        }
-
-        for(i=0;i<allOptions;i++){
-
-        const answer=document.createElement("button")
+  for (i = 0; i < allOptions; i++) {
+    /*const answer=document.createElement("button")
         
         answer.innerHTML=currentQuestion.options[i]
         
         answer.className="btn"
+        answer.onclick=getResult
         options.appendChild(answer)
-        
-        
-        
-        //console.log(currentQuestion.options[i])
-        }
 
+*/
+    /*
 
-        counter--
-    }
+console.log(options)
+console.log(currentQuestion)
+*/
 
+    options.innerHTML += `<button class="btn" id="btn${i}" onclick="getResult(${i},${currentQuestion.answer})"  >${currentQuestion.options[i]}</button>`;
+  }
 
-    
+  //console.log(currentQuestion.options[i])
 
-     
-    //------------------------------------------------------------------------------------------
-    const next=()=>{
-        if(counter===0){
-            console.log("test is over")
-        }else{
-            getQuestions()
-        }
+  counter--;
+};
 
-    }
+const getResult = (answer, correctAnswer) => {
+    console.log("getResult");
 
-    //------------------------------------------------------------------------------------------
-    window.onload=function(){
-        setAllAnimalsQuestions()
-        getQuestions()
-        
-    }
+  if (answer === correctAnswer) {
+      console.log(true)
+  document.querySelector("#btn"+answer).style.backgroundColor="green"
+  scoreCounter++
+  score=`your score is${scoreCounter}/5`
+  console.log(score)
+  
+  } else {
+      console.log(false)
+    document.querySelector("#btn"+answer).style.backgroundColor="red"
+    document.querySelector("#btn"+answer).style.color="black"
+    document.querySelector("#btn"+correctAnswer).style.backgroundColor="green"
+    score=`your score is${scoreCounter}/5`
+    console.log(score)
 
+  }
+};
+
+//------------------------------------------------------------------------------------------
+//next button
+
+const next = () => {
+  if (counter === 0) {
+    return window.location.assign("Test.html");
+  }
+
+  getQuestions();
+};
+
+//------------------------------------------------------------------------------------------
+
+window.onload = function () {
+  setAllAnimalsQuestions();
+  getQuestions();
+};
